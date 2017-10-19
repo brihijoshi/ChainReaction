028098dfa258765de67f70a1ca21147b0c8ba17e
Brihi <brihi16142@iiitd.ac.in>
2017-10-19 16:34:09 +0530

    Added Cell, Grid class in Game Engine

---
+++ trial/src/GameEngine/Grid.java
@@ -0,0 +1,14 @@
+package GameEngine;
+
+import java.util.ArrayList;
+
+public class Grid {
+
+
+    private ArrayList<ArrayList<Cell>> _grid;
+
+    public ArrayList<ArrayList<Cell>> get_grid() {
+        return _grid;
+    }
+
+}
