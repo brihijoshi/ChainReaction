e308e3cc40e1989d31fec088b175f88569ef4362
Brihi <brihi16142@iiitd.ac.in>
2017-11-13 03:52:18 +0530

    Minor refactoring

---
+++ ChainReactionGame/src/GUIEngine/restartButtonGUI.java
@@ -0,0 +1,22 @@
+package GUIEngine;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+
+public class restartButtonGUI implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+
+        MenuItem source = (MenuItem) e.getSource();
+
+        source.get
+
+
+
+    }
+}
