19a2d520726a1f3264bbc291a0040d7be8bf2548
Brihi <brihi16142@iiitd.ac.in>
2017-11-14 03:20:52 +0530

    Removed popup restart and exit bug

--- ChainReactionGame/src/GUIEngine/turnGUI.java
+++ ChainReactionGame/src/GUIEngine/turnGUI.java
@@ -159,33 +159,67 @@ class turnGUI implements EventHandler<MouseEvent> {
 
                     Optional<ButtonType> result = alert.showAndWait();
                     if (result.get() == buttonTypeOne) {
-                        ArrayList<Player> player_list = GUIMain.get_gameEngine().get_gc().get_players();
 
-                        for (Player p:
-                                player_list) {
-                            p.set_isAlive(true);
-                            p.set_isKillable(false);
-                            p.set_isActive(false);
-                        }
 
-                        player_list.get(0).set_isActive(true);
-                        player_list.get(0).set_isKillable(true);
+                        File file_game = new File("game.ser");
+                        File file_undo = new File("undo.ser");
 
-                        Color firstColor = Color.web(player_list.get(0).get_colour());
+                        try {
 
-                        for(int i = 0; i < GUIMain.get_numRows(); i++) {
-                            for (int j = 0; j < GUIMain.get_numCols(); j++) {
+                            if (file_game.delete() && file_undo.delete()) {
 
-                                GUIMain.addOrbAndAnimate(grid, i, j, 0, firstColor);
+
+                                for (int i = 0; i < players.size(); i++) {
+                                    players.get(i).set_isAlive(true);
+                                    players.get(i).set_isKillable(false);
+                                    players.get(i).set_isActive(false);
+                                }
+
+                                players.get(0).set_isActive(true);
+                                players.get(0).set_isKillable(true);
+
+                                Color firstColor = Color.web(players.get(0).get_colour());
+
+                                for (int i = 0; i < GUIMain.get_numRows(); i++) {
+                                    for (int j = 0; j < GUIMain.get_numCols(); j++) {
+
+                                        GUIMain.addOrbAndAnimate(grid, i, j, 0, firstColor);
+                                        GUIMain.get_gameEngine().get_gc().get_grid().get_grid().get(i).get(j).set_currmass(0);
+
+
+                                    }
+                                }
+
+                                GUIMain.changeGridColor(grid, firstColor);
+
+                                System.out.println("_____ inside restart handler _______");
+                                System.out.println("color of the guy that goes first" + firstColor);
+                                System.out.println(" ________ end restart handler ________");
+
+                                GUIMain.setCurrentPlayer(ColorUtil.colorToHex(firstColor));
+                                GUIMain.getRedoButton().setDisable(true);
+                                GUIMain.getRedoButton().setDisable(true);
 
                             }
+                        } catch (Exception v) {
+                            v.printStackTrace();
                         }
-
-                        GUIMain.changeGridColor(grid, firstColor);
-
                     }
                     else {
-                        stage.setScene(GUIMain.createStartPage());
+                        try {
+                            File file_game = new File("game.ser");
+
+                            File file_undo = new File("undo.ser");
+
+                            if (file_game.delete() && file_undo.delete()) {
+
+                                stage.setScene(GUIMain.createStartPage());
+                            }
+
+                        }
+                        catch (Exception o){
+                            o.printStackTrace();
+                        }
                     }
                 }
 
